61c5a4338bbe56fe90cea95bda666e48f1ecded0
Szymon Kieronski <informatyczne123@gmail.com>
2021-03-19 20:40:36 +0100

    LAB3

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+let nav = '<nav><a href = "index.html">Strona główna</a> </br>' +
+    '<a href = "contact.html">Formularz</a> </nav>';
+
+let elem = document.querySelector(".navbar");
+elem.innerHTML = nav;
